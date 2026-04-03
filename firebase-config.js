54263213a90ecbb8109d58058c958eedba9134e3
mgt581 <alexbryantwork3234@outlook.com>
2026-04-03 12:41:40 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
   apiKey: "AIzaSyDq3rozlNMYVO_9sCAqfwmnzmk67x9wN7g",
-  authDomain: "bryant0s.firebaseapp.com",
-  projectId: "bryant0s",
-  storageBucket: "bryant0s.firebasestorage.app",
+  authDomain: "bryantos.firebaseapp.com",
+  projectId: "bryantos",
+  storageBucket: "bryantos.firebasestorage.app",
   messagingSenderId: "1044948666675",
   appId: "1:1044948666675:web:d5fbefd399d7c082a85646",
   measurementId: "G-Z1CRR27XLS"
