2e39c46baeb30849086892433e497736b64850f5
mgt581 <alexbryantwork3234@outlook.com>
2026-04-04 03:56:22 +0100

    Enable browser local persistence for auth
    Added persistence setting for Firebase authentication.

--- firebase-init.js
+++ firebase-init.js
@@ -1,9 +1,17 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
-import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
-import { firebaseConfig } from "./firebase-config.js"; // ✅ FIXED NAME
+import {
+  getAuth,
+  GoogleAuthProvider,
+  setPersistence,
+  browserLocalPersistence
+} from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
+import { firebaseConfig } from "./firebase-config.js";
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+
+await setPersistence(auth, browserLocalPersistence);
+
 const provider = new GoogleAuthProvider();
 
 provider.setCustomParameters({
