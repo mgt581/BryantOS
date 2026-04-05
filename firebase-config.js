1ca61ec43ef8c6baa6728192f96bd33b3b52a9ac
mgt581 <alexbryantwork3234@outlook.com>
2026-04-05 02:10:54 +0100

    Update storageBucket in firebaseConfig

--- firebase-config.js
+++ firebase-config.js
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyDq3rozlNMYVO_9sCAqfwmnzmk67x9wN7g",
   authDomain: "bryantos.firebaseapp.com",
   projectId: "bryantos",
-  storageBucket: "bryantos.firebasestorage.app",
+  storageBucket: "bryantos.appspot.com",
   messagingSenderId: "1044948666675",
   appId: "1:1044948666675:web:d5fbefd399d7c082a85646",
   measurementId: "G-Z1CRR27XLS"
