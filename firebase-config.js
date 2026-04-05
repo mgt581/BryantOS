3c9260051c00bf3845e5b938391e0d97025576f2
mgt581 <alexbryantwork3234@outlook.com>
2026-04-05 12:57:11 +0100

    Fix firebase config for updated project details
    Updated firebase configuration details to reflect new project settings.

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
-export const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyDq3rozlNMYVO_9sCAqfwmnzmk67x9wN7g",
-  authDomain: "bryantos.firebaseapp.com",
-  projectId: "bryantos",
-  storageBucket: "bryantos.appspot.com",
+  authDomain: "bryant0s.firebaseapp.com",
+  projectId: "bryant0s",
+  storageBucket: "bryant0s.appspot.com", // ✅ FIXED
   messagingSenderId: "1044948666675",
   appId: "1:1044948666675:web:d5fbefd399d7c082a85646",
   measurementId: "G-Z1CRR27XLS"
